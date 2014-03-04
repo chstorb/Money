d9bfa0f9f54b765f460561ef6cc43a7b8b6fdc5c
chstorb_cp <SND\chstorb_cp>
2014-03-04 22:51:46 +0000

    Projekt angelegt

---
+++ tools/GenerateCodeFiles.cmd
@@ -0,0 +1,5 @@
+@echo off
+set WindowsSdkDir=C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\
+"%WindowsSdkDir%bin\NETFX 4.5.1 Tools\xsd" /p:param.pain.001.003.03.xml 
+"%WindowsSdkDir%bin\NETFX 4.5.1 Tools\xsd" /p:param.pain.002.003.03.xml
+"%WindowsSdkDir%bin\NETFX 4.5.1 Tools\xsd" /p:param.pain.008.003.02.xml
