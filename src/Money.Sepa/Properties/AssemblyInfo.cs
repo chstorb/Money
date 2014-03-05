ea11f09ff0819236caec7529d1a79eac2dd39cca
chstorb_cp <SND\chstorb_cp>
2014-03-05 21:56:48 +0000

    Jahresangabe hinzugefügt

--- src/Money.Sepa/Properties/AssemblyInfo.cs
+++ src/Money.Sepa/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("STC STORB Consulting")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright © STC STORB Consulting 2014")]
+[assembly: AssemblyCopyright("Copyright 2014 © STC STORB Consulting 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
