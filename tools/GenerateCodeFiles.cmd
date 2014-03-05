089a8ea30b4aecee4bef8c0d976c42ce53045e68
Christian Storb <chstorb@users.noreply.github.com>
2014-03-05 21:48:21 +0000

    Erweitert

--- tools/GenerateCodeFiles.cmd
+++ tools/GenerateCodeFiles.cmd
@@ -3,3 +3,7 @@ set WindowsSdkDir=C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\
 "%WindowsSdkDir%bin\NETFX 4.5.1 Tools\xsd" /p:param.pain.001.003.03.xml 
 "%WindowsSdkDir%bin\NETFX 4.5.1 Tools\xsd" /p:param.pain.002.003.03.xml
 "%WindowsSdkDir%bin\NETFX 4.5.1 Tools\xsd" /p:param.pain.008.003.02.xml
+"%WindowsSdkDir%bin\NETFX 4.5.1 Tools\xsd" /p:param.camt.052.001.02.xml
+"%WindowsSdkDir%bin\NETFX 4.5.1 Tools\xsd" /p:param.camt.053.001.02.xml
+"%WindowsSdkDir%bin\NETFX 4.5.1 Tools\xsd" /p:param.camt.054.001.02.xml
+
