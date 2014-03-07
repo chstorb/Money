3292600d2dfd08ff6c90cb5ae8a10e2f25cc604b
chstorb_cp <SND\chstorb_cp>
2014-03-07 21:08:08 +0000

    Änderungen/Ergänzungen vorgenommen

--- tools/GenerateCodeFiles.cmd
+++ tools/GenerateCodeFiles.cmd
@@ -1,9 +1,16 @@
-@echo off
-set WindowsSdkDir=C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\
-"%WindowsSdkDir%bin\NETFX 4.5.1 Tools\xsd" /p:param.pain.001.003.03.xml 
-"%WindowsSdkDir%bin\NETFX 4.5.1 Tools\xsd" /p:param.pain.002.003.03.xml
-"%WindowsSdkDir%bin\NETFX 4.5.1 Tools\xsd" /p:param.pain.008.003.02.xml
-"%WindowsSdkDir%bin\NETFX 4.5.1 Tools\xsd" /p:param.camt.052.001.02.xml
-"%WindowsSdkDir%bin\NETFX 4.5.1 Tools\xsd" /p:param.camt.053.001.02.xml
-"%WindowsSdkDir%bin\NETFX 4.5.1 Tools\xsd" /p:param.camt.054.001.02.xml
+@if "%WindowsSDK_ExecutablePath_x64%"=="" goto error_no_WindowsSDK_ExecutablePath_x64
 
+"%WindowsSDK_ExecutablePath_x64%\xsd" /p:param.camt.052.001.02.xml
+"%WindowsSDK_ExecutablePath_x64%\xsd" /p:param.camt.052.001.03.xml
+"%WindowsSDK_ExecutablePath_x64%\xsd" /p:param.camt.053.001.02.xml
+"%WindowsSDK_ExecutablePath_x64%\xsd" /p:param.camt.054.001.02.xml
+"%WindowsSDK_ExecutablePath_x64%\xsd" /p:param.pain.001.003.03.xml 
+"%WindowsSDK_ExecutablePath_x64%\xsd" /p:param.pain.002.003.03.xml
+"%WindowsSDK_ExecutablePath_x64%\xsd" /p:param.pain.008.003.02.xml
+
+@goto end
+
+:error_no_WindowsSDK_ExecutablePath_x64
+@echo ERROR: You must run this command in a Developer Command Prompt for Visual Studio
+
+:end
