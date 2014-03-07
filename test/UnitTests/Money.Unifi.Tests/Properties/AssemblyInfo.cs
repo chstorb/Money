535fd706eaefb1e5dfe157edb0b18a550e852c21
chstorb_cp <SND\chstorb_cp>
2014-03-07 22:20:55 +0000

    Testprojekt hinzugefügt

---
+++ test/UnitTests/Money.Unifi.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über folgende 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("Money.Unifi.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Money.Unifi.Tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Wenn ComVisible auf "false" festgelegt wird, sind die Typen innerhalb dieser Assembly 
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("d3026159-3ea9-4b25-8553-6c909f3a82c1")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// durch Einsatz von '*', wie in nachfolgendem Beispiel:
+// [Assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ test/UnitTests/Money.Unifi.Tests/SepaCreditTransferTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml.Serialization;
+using System.Net;
+using System.IO;
+
+namespace Money.Unifi.Tests
+{
+    [TestClass]
+    public class SepaCreditTransferTests
+    {
+        [TestMethod]
+        public void DeserializePaymentInstructionInformations()
+        {
+            Uri uri = new Uri("http://www.ebics.de/fileadmin/unsecured/anlage3/anlage3_pain001/pain_ex/pain.001.003.03.xml");
+
+            XmlSerializerFactory factory = new XmlSerializerFactory();
+            XmlSerializer ser = factory.CreateSerializer(typeof(Money.Unifi.pain_001_003_03.Document));
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream responseStream = response.GetResponseStream();
+            Money.Unifi.pain_001_003_03.Document doc = (Money.Unifi.pain_001_003_03.Document)ser.Deserialize(responseStream);
+            responseStream.Close();
+
+            Assert.IsNotNull(doc);
+            Assert.AreEqual("Message-ID-4711", doc.CstmrCdtTrfInitn.GrpHdr.MsgId);
+
+            Console.WriteLine("MsgId = {0}, InitgPty.Nm = {1}", doc.CstmrCdtTrfInitn.GrpHdr.MsgId, doc.CstmrCdtTrfInitn.GrpHdr.InitgPty.Nm);
+
+            foreach (Money.Unifi.pain_001_003_03.PaymentInstructionInformationSCT entry in doc.CstmrCdtTrfInitn.PmtInf)
+            {
+                Console.WriteLine("CtrlSum = {0}, NbOfTxs = {1}", entry.CtrlSum, entry.NbOfTxs);
+            }
+        }
+    }
+}
